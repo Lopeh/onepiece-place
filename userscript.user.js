98fe0437c2691f711b43794f3027713d82301df2
Lopeh <Thaipomail@gmail.com>
2022-04-04 12:23:40 -0400

    window maybe?

--- userscript.user.js
+++ userscript.user.js
@@ -10,13 +10,13 @@
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
             i.src = "https://raw.githubusercontent.com/lopeh/onepiece-place/main/onepiece_template.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
-            document.addEventListener("keydown", function(event) {                
+            window.addEventListener("keydown", function(event) {                
                 // Hide the image with the F4 key
                 if(event.key == "F4"){
                     console.log('showing/hiding/refreshing onepiece...');
